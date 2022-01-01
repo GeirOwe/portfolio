4c05ba564214dad4b47cce57a6e2f31fbd85fe6e
GeirOwe <geir.owe@hotmail.com>
2022-01-01 20:41:44 +0100

    added gunicorn

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
 LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+# Bind to PORT if defined, otherwise default to 5000.
+port = int(os.environ.get('PORT', 5000))
