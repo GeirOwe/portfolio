843f49ab7205e1cb8549e13b95ec117f357f06d1
GeirOwe <geir.owe@hotmail.com>
2022-01-01 20:00:17 +0100

    added comments

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,9 @@ app.config.from_object(Config)
 from app import routes
 bootstrap = Bootstrap(app)
 
-# (venv) $ pip freeze > requirements.txt
+# (virtu) $ pip freeze > requirements.txt
+# (virtu) geirowe@geirs-imac portfolio % heroku apps:create garyoinvest
+# Creating ⬢ garyoinvest... done
+# https://garyoinvest.herokuapp.com/ | https://git.heroku.com/garyoinvest.git
+# To push the current branch and set the remote as upstream, use
+# git push --set-upstream heroku master
