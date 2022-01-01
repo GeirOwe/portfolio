91b120e01dddbdc15c64ccaf0b8560ee1e61c1ff
GeirOwe <geir.owe@hotmail.com>
2022-01-01 21:14:45 +0100

    working the PORT issue

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # Bind to PORT if defined, otherwise default to 5000.
+    PORT = int(os.environ.get('PORT', 5000))
 
 LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-# Bind to PORT if defined, otherwise default to 5000.
-port = int(os.environ.get('PORT', 5000))
